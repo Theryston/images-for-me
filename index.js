105b1a70a6cb068d648bfd16284e6bcabf875bf7
Theryston <funktodo2@gmail.com>
2020-12-12 15:23:28 -0300

    init

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const google = require('googleapis').google
 const customSearch = google.customsearch('v1');
-const download = require('image-downloader')
+const download = require('image-downloader');
+var readlineSync = require('readline-sync');
+const path = require('path')
 
 
 const googlesearchcredencials = require('./credentials/google-search.json');
@@ -35,32 +37,77 @@ async function search(search) {
 	}
 }
 
-async function downloading(UrlsImgs) {
+var imgTotals = 0
 
-	UrlsImgs.forEach((UrlImg) => {
+async function downloading(UrlsImgs, index, limit, term, dest) {
 
-		const options = {
-			url: UrlImg.link,
-			dest: './download/'
-		}
+	const options = {
+		url: UrlsImgs[index].link,
+		dest: dest+Date.now().toString()+' - '+term+path.extname(UrlsImgs[index].link)
+	}
 
+	if (index < limit) {
 		download.image(options).then(() => {
-			console.log('Saved')
+			if (index > 0) {
+				console.log('Another downloaded image')
+			} else {
+				console.log('I downloaded the first image')
+			}
+			imgTotals++
+
+			downloading(UrlsImgs, index+1, limit, term, dest)
 		}).catch(() => {
-			console.log('error in save')
+			console.log('There was an error downloading one of the images but I will download another!\n')
+			downloading(UrlsImgs, index+1, limit, term, dest)
 		})
-	})
+
+
+	} else {
+		console.log('Perfect! the limit of images to be downloaded has already been reached\n')
+		console.log('total images downloaded: '+imgTotals)
+	}
 }
 
 
 
 void async function() {
-	const {response, items} = await search(process.argv[2])
+	var term = readlineSync.question('Enter a term: ')
+
+
+	const {
+		response,
+		items
+	} = await search(term)
 
 	if (items !== 'I did not find images related to this term') {
 		console.log(`Ok! I found ${items.length} images`)
 
-		downloading(items)
+		var limit = readlineSync.question('How many images do you want me to download: ')
+
+		if (isNaN(limit)) {
+			console.log('You need to enter a number.')
+
+			limit = readlineSync.question('how many images do you want me to download: ')
+		}
+
+		if (Number(limit) > items.length) {
+			console.log('The number of images to be downloaded cannot be greater than the number of images I found\n')
+
+			limit = readlineSync.question('how many images do you want me to download: ')
+		}
+		
+		var dest = readlineSync.question(`\nEnter the exact destination where the images will be saved.\nExample: /desktop/folder-for-downloaded-images/\nIf you want to download it in the folder you are in, type '.'\ndest: `)
+		
+		
+		if (dest == '.') {
+			dest = './'
+		}
+		
+		if (dest.substring(dest.length-1, dest.length).indexOf('/') == -1) {
+			dest = dest+'/'
+		}
+
+		downloading(items, 0, limit, term, dest)
 
 	} else {
 		console.log(items)
